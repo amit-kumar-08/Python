bf31ae3bc25136f962a5d6b2dc705682f01c7274
amit-kumar-08 <amit.mukul.cg@gmail.com>
2026-08-21 17:55:04 +0530

    added welcome.py and about_python.py

---
+++ Introduction to python/about_python.py
@@ -0,0 +1,2 @@
+#python is high level programming language that is easy to learn.
+print("I am learning Python and enjoying programming!")
---
+++ Introduction to python/welcome.py
@@ -0,0 +1,2 @@
+#welcome message
+print("welcome to the python")
