4c06b7e11ff5a9cdadf2bb20a62d40bd4146c742
amit-kumar-08 <amit.mukul.cg@gmail.com>
2026-08-22 16:54:23 +0530

    variables.py added

---
+++ Variables/variables.py
@@ -0,0 +1,54 @@
+#PRACTICAL PROBLEMS
+#====================== Question 24 ============================
+my_name = "Amit kumar"
+my_age = 18
+my_city = "Bangalore"
+
+print(my_name)
+print(my_age)
+print(my_city)
+
+#========================== Question 25 ===============================
+student_name = "Aarav"
+student_roll_number = 18
+student_branch = "B.Tech CSE-AI"
+
+print(student_name)
+print(student_roll_number)
+print(student_branch)
+
+#========================== Question 26 ================================
+marks = 99
+marks = 100
+
+print(marks)
+
+#========================== Question 27 =================================
+name, age, city = "Rahul", 18, "Patna"
+
+print(name,age,city)
+
+#========================== Question 28 ==================================
+x = y = z = 0
+
+print(x,y,z)
+
+#========================== Question 29 ===================================
+student1 = "Rahul"
+student_name_ = "Rahul"
+course = "B.Tech"
+
+print(student1)
+print(student_name_)
+print(course)
+
+#=========================== Question 30 ===================================
+
+#Student information
+student_name1 = "Shivam"
+age_ = 18
+
+age_ = 20
+
+print(student_name1)
+print(age_)
