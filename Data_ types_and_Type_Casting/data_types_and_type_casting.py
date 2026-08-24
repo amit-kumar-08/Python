127d01353e7c41ace061d548c3ed7fb82d80ef53
amit-kumar-08 <amit.mukul.cg@gmail.com>
2026-08-24 17:55:11 +0530

    data_types_and_type_casting.py file added

---
+++ Data_ types_and_Type_Casting/data_types_and_type_casting.py
@@ -0,0 +1,67 @@
+#Practical Problems
+#======================== Question 24 ============================
+
+#student information
+student_name = "Amit"
+student_age = 18
+student_height = 6.0
+is_student = True
+student_result = None
+
+print(student_name,type(student_name),student_age,type(student_age),student_height,type(student_height),is_student,type(is_student),student_result,type((student_result)))
+
+
+#======================== Question 25 ============================
+
+a = 50
+b = 50.0
+c = "50"
+
+print(a,type(a),b,type(b),c,type(c))
+
+
+#======================== Question 26 ============================
+
+A = True
+B = "True"
+
+print(A,type(A),B,type(B))
+
+#======================== Question 27 ============================
+
+x = None
+y = "None"
+
+print(x,type(x),y,type(y))
+
+#======================== Question 28 ============================
+
+value = 100
+value = "100"
+print(type(value))
+
+#======================== Question 29 ============================
+
+#Product information
+product_name = "Modern Table"
+product_quantity = 50
+product_price = 2500.00
+product_available = True
+product_discount = None
+
+print(product_name,type(product_name),product_quantity,type(product_quantity),product_price,type(product_price),product_available,type(product_available),product_discount,type(product_discount))
+
+#======================== Question 30 ============================
+
+num = 10          #int data_type
+num1 = 10.0       #floot data_type
+num3 = "10"       #str data_type
+
+num4 = True       #bool data_type
+num5 = "True"     #str data_type because 'True' is inside double quote
+
+num6 = None       #NoneType data_type
+num7 = "None"     #str data_type same as num5 variable
+
+
+print(num,type(num),num1,type(num1),num3,type(num3),num4,type(num4),num5,type(num5),num6,type(num6),num7,type(num7))
